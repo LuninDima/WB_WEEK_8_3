4cf90e68ce235ed167b25f34632eac741a273e37
moondi <moon-di-2009@yandex.ru>
2022-06-21 18:20:37 +0300

    Обновление данных на экране About

---
+++ app/src/main/java/com/example/wb_week_8_3/view/about/AboutFragment.kt
@@ -0,0 +1,32 @@
+package com.example.wb_week_8_3.view.about
+
+import android.graphics.Color
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.wb_week_8_3.databinding.FragmentAboutBinding
+
+
+class AboutFragment : Fragment() {
+
+    private var _binding: FragmentAboutBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAboutBinding.inflate(layoutInflater, container, false)
+        setupHyperlink()
+        return binding.root
+    }
+
+    private fun setupHyperlink() {
+        binding.tvGitHub.movementMethod = LinkMovementMethod.getInstance()
+        binding.tvGitHub.setLinkTextColor(Color.BLUE)
+    }
+}
